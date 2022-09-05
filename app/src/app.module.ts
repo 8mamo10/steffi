24cb41478d8293ab0dbd3f8dec89c4bc2ab3850d
Hiroki Yamamoto <562105+8mamo10@users.noreply.github.com>
2022-09-05 23:34:52 +0900

    Add post resolver

--- app/src/app.module.ts
+++ app/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PostResolver } from './post/post.resolver';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PostResolver],
 })
 export class AppModule {}
