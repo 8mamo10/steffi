b6373453812ada154ba18ae4bdb204d446d2f1b7
Hiroki Yamamoto <562105+8mamo10@users.noreply.github.com>
2022-10-08 17:40:04 +0900

    Store DATABASE_URL to secret manager

--- infra/terraform/modules/cloudrun/steffi_service.tf
+++ infra/terraform/modules/cloudrun/steffi_service.tf
@@ -7,6 +7,16 @@ resource "google_cloud_run_service" "steffi" {
     spec {
       containers {
         image = var.container_images.steffi
+        env {
+          name = "DATABASE_URL"
+
+          value_from {
+            secret_key_ref {
+              key  = "latest"
+              name = "DATABASE_URL"
+            }
+          }
+        }
       }
     }
   }
