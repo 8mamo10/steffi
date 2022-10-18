c2bcad15fc6aa0381f0ce0c88ab3fbd2e66a6def
Hiroki Yamamoto <562105+8mamo10@users.noreply.github.com>
2022-10-18 23:57:43 +0900

    Add google_sql_database_instance

--- infra/terraform/modules/cloudsql/database.tf
+++ infra/terraform/modules/cloudsql/database.tf
@@ -5,3 +5,14 @@ resource "google_sql_database" "steffi" {
   charset   = "UTF8"
   collation = "en_US.UTF8"
 }
+
+resource "random_id" "db_name_suffix" {
+  byte_length = 4
+}
+
+resource "google_sql_database_instance" "master" {
+  database_version = "POSTGRES_14"
+  name             = "steffi-${random_id.db_name_suffix.hex}"
+  project          = var.project_id
+  region           = var.region
+}
