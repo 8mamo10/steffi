0d30162c10e4e19c559ff706cf53fd44a203e60f
Hiroki Yamamoto <562105+8mamo10@users.noreply.github.com>
2022-10-26 23:56:08 +0700

    Update terraform for databases, but not enough

--- infra/terraform/modules/cloudsql/database.tf
+++ infra/terraform/modules/cloudsql/database.tf
@@ -1,18 +1,19 @@
-resource "google_sql_database" "steffi" {
-  name      = "steffi"
-  project   = var.project_id
-  instance  = "steffi"
-  charset   = "UTF8"
-  collation = "en_US.UTF8"
-}
-
-resource "random_id" "db_name_suffix" {
-  byte_length = 4
-}
-
-resource "google_sql_database_instance" "master" {
+resource "google_sql_database_instance" "steffi" {
   database_version = "POSTGRES_14"
-  name             = "steffi-${random_id.db_name_suffix.hex}"
   project          = var.project_id
   region           = var.region
+
+  settings {
+    tier = "db-f1-micro"
+  }
+}
+
+resource "google_sql_database" "docker" {
+  depends_on = [google_sql_database_instance.steffi]
+
+  name      = "docker"
+  project   = var.project_id
+  instance  = google_sql_database_instance.steffi.name
+  charset   = "UTF8"
+  collation = "en_US.UTF8"
 }
